98c09d4533a7f01a015a2ba3719ed91c6baa0a7f
hotaka <s1f102103864@iniad.org>
2025-05-08 21:24:28 +0900

    Scenedelegate削除

---
+++ RxSwiftPrac/RxSwiftPrac/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  RxSwiftPrac
+//
+//  Created by 金澤帆高 on 2025/05/08.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        let viewController = RootViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
